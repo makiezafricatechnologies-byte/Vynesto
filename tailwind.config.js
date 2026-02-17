d19ffc9400832597e3b359afe635142d93e7fc33
makiezafricatechnologies-byte <makiezafricatechnologies@gmail.com>
2026-02-17 12:07:41 +0300

    Create Tailwind configuration for Vynesto with violet color scheme

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/**
+ * Tailwind CSS configuration file
+ * See https://tailwindcss.com/docs/configuration for details
+ */
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        violet: {
+          primary: '#8A3FFC', // Example primary violet color
+          secondary: '#5C2E9C', // Example secondary violet color
+        },
+        shopping: {
+          primary: '#8A3FFC',
+          secondary: '#5C2E9C',
+        },
+        wellness: {
+          primary: '#8A3FFC',
+          secondary: '#5C2E9C',
+        },
+        campus: {
+          primary: '#8A3FFC',
+          secondary: '#5C2E9C',
+        },
+        gigs: {
+          primary: '#8A3FFC',
+          secondary: '#5C2E9C',
+        },
+      },
+    },
+  },
+};
