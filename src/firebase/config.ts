83894227f858218391d3e2b54e4e157a9d96e202
Makiezafrica <makiezafricatechnologies@gmail.com>
2026-02-21 15:23:23 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebasestorage.app`,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-6627816252-28483",
+  "appId": "1:466804191797:web:6b036fabb407a9e548b77c",
+  "apiKey": "AIzaSyBJwAYskMXS0hM1-V0BxUtIv0l7mJb5iHg",
+  "authDomain": "studio-6627816252-28483.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "466804191797"
 };
